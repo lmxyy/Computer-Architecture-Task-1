76bec1693794e48c1ae876780f80b32fcd22afe5
lmxyy <664789531@qq.com>
2017-10-29 17:54:28 +0800

    finished add.v

--- Simulation Source/adder.v
+++ Simulation Source/adder.v
@@ -1,28 +1,18 @@
-/* ACM Class System (I) 2017 Fall Homework 1 
- *
- * Part I: Write an adder in Verilog
- *
- * Implement your naive adder here
- * 
- * GUIDE:
- *   1. Create a RTL project in Vivado
- *   2. Put this file into `Sources'
- *   3. Put `test_adder.v' into `Simulation Sources'
- *   4. Run Behavioral Simulation
- *   5. Make sure to run at least 100 steps during the simulation (usually 100ns)
- *   6. You can see the results in `Tcl console'
- *
- */
+module adder(x,y,sum);
+   parameter maxn = 16;
 
-module adder(
-	// TODO: Write the ports of this module here
-	//
-	// Hint: 
-	//   The module needs 3 ports, 
-	//     the first 2 ports are 16-bit unsigned numbers as the inputs of the adder
-	//     the third port is a 16-bit unsigned number as the output
-	// 
-);
-	// TODO: Implement this module here
-	
+   input [maxn-1:0] x,y
+   output [maxn-1:0] sum;
+
+   reg 				 C[maxn:0];
+   always @(x,y)
+	 begin
+		C[0] = 0
+		integer i;
+		for (i = 0;i < maxn;i = i+1)
+		  begin
+			 C[i] = x[i]^y[i]^C[i];
+			 C[i+1] = (x[i]&y[i])^(C[i]&(x[i]^y[i]));
+		  end
+	 end		
 endmodule
